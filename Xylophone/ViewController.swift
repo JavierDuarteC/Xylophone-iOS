ef626d541b0efbeb78472ab525f9b1f4e10da924
Javier Duarte <javierandresduarte@gmail.com>
2022-11-03 11:56:33 -0500

    added animations on key pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    
     @IBAction func keyPressed(_ sender: UIButton) {
+        print("start")
+        sender.alpha = 0.5
         playSound(note: sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){
+            print("End")
+            sender.alpha = 1
+        }
     }
     
     func playSound(note: String){
