9dc9e052b1d1b7d434ab20f0c357c0697ccfe8ae
Javier Duarte <javierandresduarte@gmail.com>
2022-11-03 12:18:39 -0500

    code formatted with swimat

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    
+
     var player: AVAudioPlayer!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
+
+
     @IBAction func keyPressed(_ sender: UIButton) {
         print("start")
         sender.alpha = 0.5
         playSound(note: sender.currentTitle!)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             print("End")
             sender.alpha = 1
         }
     }
-    
-    func playSound(note: String){
+
+    func playSound(note: String) {
         let url = Bundle.main.url(forResource: note, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
